2adf15574118ac523875b9012985ae72c06a5427
Vijay Kumar <vijaychan96@gmail.com>
2024-03-22 14:57:44 +0530

    added http post

--- src/components/DetailsCard.js
+++ src/components/DetailsCard.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import SegmentCard from "./SegmentCard";
+import axios from "axios"
 
-function DetailsCard({setIshow}) {
+function DetailsCard({ setIshow }) {
     const [segName, setSegName] = useState("")
     const [selectedSchema, setSelectedSchema] = useState("")
     const [selectedValue, setSelectedValue] = useState("")
@@ -34,7 +35,10 @@ function DetailsCard({setIshow}) {
         setSegments([...segments, obj])
     }
     const handleSaveClick = () => {
-        console.log(JSON.stringify({ segment_name: segName, schema: selectedSchema }))
+        const finalData =  JSON.stringify({ segment_name: segName, schema: selectedSchema })
+        // console.log(finalData)
+        axios.post("https://webhook.site/cd0bc93c-3d6e-42d3-9741-b48f54094ee1",finalData).then((res)=>{console.log(res)})
+        .catch((err)=>{console.log(err.message)})
     }
 
 
@@ -44,34 +48,34 @@ function DetailsCard({setIshow}) {
                 <b > &lt; Saving Segment</b>
             </div>
             <div className="p-6" >
-            <label>Enter the Name of the Segment</label> <br />
-            <input
-                className="my-4 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-black"
-                placeholder="Name of the Segment"
-                type="text"
-                onChange={handleNameChange} />
-            <p className="py-4" >To save you segment, you need to add the schemas to build the query</p>
-            {segments && segments.map((ele, i) => {
-                return <SegmentCard key={i + 1} data={ele.data} />
-            })}
-            <select
-                className="my-4 w-96 h-12 border border-black"
-                onChange={handlChange}  >
-                <option value="">Add Schema to segment</option>
-                {
-                    (data.length != 0 ? data : arr).map((ele) => {
-                        return (<option key={ele.id} value={ele.value} >{ele.name}</option>)
-                    })
-                }
-            </select> <br />
-            <a className="text-green-400 underline underline-offset-4" onClick={handleClick} >+ Add new schema</a><br />
+                <label>Enter the Name of the Segment</label> <br />
+                <input
+                    className="my-4 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-black"
+                    placeholder="Name of the Segment"
+                    type="text"
+                    onChange={handleNameChange} />
+                <p className="py-4" >To save you segment, you need to add the schemas to build the query</p>
+                {segments && segments.map((ele, i) => {
+                    return <SegmentCard key={i + 1} data={ele.data} />
+                })}
+                <select
+                    className="my-4 w-96 h-12 border border-black"
+                    onChange={handlChange}  >
+                    <option value="">Add Schema to segment</option>
+                    {
+                        (data.length != 0 ? data : arr).map((ele) => {
+                            return (<option key={ele.id} value={ele.value} >{ele.name}</option>)
+                        })
+                    }
+                </select> <br />
+                <a className="text-green-400 underline underline-offset-4" onClick={handleClick} >+ Add new schema</a><br />
             </div>
             <div className="w-full mt-12 py-8 pl-6  bg-gray-200" >
                 <button
                     className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
                     onClick={handleSaveClick} >Save the segment</button>
                 <button
-                onClick={()=>{setIshow(false)}}
+                    onClick={() => { setIshow(false) }}
                     className="text-black hover:bg-green-700 hover:text-white  border border-black font-bold py-2 px-4 mx-2 rounded text-red-700"
                 >Cancel</button>
             </div>
